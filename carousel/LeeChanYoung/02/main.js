d0e0b3dfca1be181ee63a7627f620365a201f069
Dokimion0 <leecy3610@gmail.com>
2023-01-29 23:03:51 +0900

    feat: Add carousel

--- carousel/LeeChanYoung/02/main.js
+++ carousel/LeeChanYoung/02/main.js
@@ -1,97 +1,48 @@
-let sliderContainer = document.querySelector(".slider-container");
-let innerSlider = document.querySelector(".inner-slider");
+let sliderContainer = document.querySelector('.slider-container');
+let innerSlider = document.querySelector('.inner-slider');
 
 let startX;
 let x;
+let scrollLeft;
 let pressed = false;
 
-sliderContainer.addEventListener("mousedown", (e) => {
+sliderContainer.addEventListener('mousedown', (e) => {
   pressed = true;
+  startX = e.pageX;
+  scrollLeft = sliderContainer.scrollLeft;
 
-  startX = e.offsetX - innerSlider.offsetLeft;
-  let outer = sliderContainer.getBoundingClientRect();
-  let inner = innerSlider.getBoundingClientRect();
-
-
-  sliderContainer.style.cursor = "grabbing";
+  sliderContainer.style.cursor = 'grabbing';
 });
 
-sliderContainer.addEventListener("mousemove", (e) => {
+sliderContainer.addEventListener('mousemove', (e) => {
   if (!pressed) return;
   e.preventDefault();
 
-  x = e.offsetX;
-  innerSlider.style.left = `${x - startX}px`;
+  const x = e.pageX;
+  const scroll = x - startX;
+  sliderContainer.scrollLeft = scrollLeft - scroll;
+
+  console.log(scrollLeft, scroll);
+  console.log(sliderContainer.scrollLeft);
+  // autoSlide();
 });
 
-sliderContainer.addEventListener("mouseenter", () => {
-  sliderContainer.style.cursor = "grab";
+sliderContainer.addEventListener('mouseenter', () => {
+  sliderContainer.style.cursor = 'grab';
 });
 
-sliderContainer.addEventListener("mouseup", () => {
-  checkBoundary();
+sliderContainer.addEventListener('mouseup', () => {
   pressed = false;
-  sliderContainer.style.cursor = "grab";
+  sliderContainer.style.cursor = 'grab';
 });
 
-sliderContainer.addEventListener("mouseleave", () => {
-  checkBoundary();
+sliderContainer.addEventListener('mouseleave', () => {
   pressed = false;
-  sliderContainer.style.cursor = "grab";
+  sliderContainer.style.cursor = 'grab';
 });
 
-const checkBoundary = () => {
-  let outer = sliderContainer.getBoundingClientRect();
-  let inner = innerSlider.getBoundingClientRect();
-
-  const itemWidth = inner.width / 6;
-  let dragWidth = 0;
-  dragWidth += startX - x;
-
-  // if (parseInt(innerSlider.style.left) > 0) {
-  //   innerSlider.style.left = "0px";
-  // }
-
-  if (dragWidth < 0) {
-    innerSlider.style.left = `0px`;
-    dragWidth = 0;
-  } else if (dragWidth < itemWidth) {
-    innerSlider.style.left = `-${itemWidth}px`;
-    dragWidth = itemWidth;
-  } else if (dragWidth < itemWidth * 2) {
-    innerSlider.style.left = `-${itemWidth * 2}px`;
-    dragWidth = itemWidth * 2;
-  } else if (dragWidth < itemWidth * 3) {
-    innerSlider.style.left = `-${itemWidth * 3}px`;
-    dragWidth = itemWidth * 3;
-  } else if (dragWidth < itemWidth * 4) {
-    innerSlider.style.left = `-${itemWidth * 4}px`;
-    dragWidth = itemWidth * 4;
-  } else if (dragWidth < itemWidth * 5) {
-    innerSlider.style.left = `-${itemWidth * 5}px`;
-    dragWidth = itemWidth * 5;
-  } else if (dragWidth < itemWidth * 6) {
-    innerSlider.style.left = `-${itemWidth * 6}px`;
-    dragWidth = itemWidth * 6;
+const autoSlide = () => {
+  if (0 < sliderContainer.scrollLeft) {
+    sliderContainer.scrollLeft = 200;
   }
-
-  console.log(dragWidth);
-  // else if (dragWidth < itemWidth * 3) {
-  //   innerSlider.style.left = `-${itemWidth * 3}px`;
-  // } else if (dragWidth < itemWidth * 4) {
-  //   innerSlider.style.left = `-${itemWidth * 4}px`;
-  // } else if (dragWidth < itemWidth * 5) {
-  //   innerSlider.style.left = `-${itemWidth * 5}px`;
-  // } else if (dragWidth < itemWidth * 6) {
-  //   innerSlider.style.left = `-${itemWidth * 6}px`;
-  // }
-
-  // } else if (itemWidth * 2 < dragWidth) {
-  //   innerSlider.style.left = `${itemWidth * 2}px`;
-  // } else if (itemWidth * 3 < dragWidth) {
-  //   innerSlider.style.left = `${itemWidth * 3}px`;
 };
-
-// if (inner.right < outer.right) {
-//   innerSlider.style.left = `-${inner.width - outer.width}px`;
-// }
