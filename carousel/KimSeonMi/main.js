3539252ae83534d8f348e205a6b2acd59ed95771
xiuxiuboK <tjsalsun@gmail.com>
2023-02-02 20:36:37 +0900

    feat: Edit main.js

--- carousel/KimSeonMi/main.js
+++ carousel/KimSeonMi/main.js
@@ -1,11 +1,34 @@
 const activeClass =  '_active'
+const opacityClass =  '_opacity'
 
-const slidesEls = document.querySelectorAll('silde_item')
+const slidesEls = document.querySelectorAll('.slide_item')
 const arrSlidesEls = Array.from(slidesEls)
-
+const rigthEl = document.querySelector('.right')
+const leftEl = document.querySelector('.left')
+console.log(arrSlidesEls)
 let curIndex = 0
 
 const moveRigthSlide = () => {
   arrSlidesEls[curIndex].classList.remove(activeClass)
+  
+  if(curIndex < arrSlidesEls.length - 1){
+    curIndex++
+  }else{
+    curIndex = 0
+  }
+  arrSlidesEls[curIndex].classList.add(activeClass)
+}
+const moveLeftSlide = () => {
+  
+  arrSlidesEls[curIndex].classList.remove(activeClass)
+  
+  if(curIndex > 0){
+    curIndex--
+  }else{
+    curIndex = arrSlidesEls.length - 1
+  }
+  arrSlidesEls[curIndex].classList.add(activeClass)
 }
 
+rigthEl.addEventListener('click', moveRigthSlide)
+leftEl.addEventListener('click', moveLeftSlide)
