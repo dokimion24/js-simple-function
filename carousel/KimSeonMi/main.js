c6450d0c54b306f507f60852a3d643c8a7d6b162
xiuxiuboK <tjsalsun@gmail.com>
2023-02-02 13:00:23 +0900

    feat: Add content in main.js

---
+++ carousel/KimSeonMi/main.js
@@ -0,0 +1,8 @@
+const containerEl = document.querySelector('.slide-container')
+const slideFristEl = document.querySelector('.slide-container > div')
+const slideSecondEl = document.querySelector('.slide-container>:nth-child(2)')
+const slideThirdEl = document.querySelector('.slide-container>:last-child')
+
+
+
+
